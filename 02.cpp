cab556708ced209b61ba3b6654b887d9e4f28a18
wulu <wulu90@outlook.com>
2025-03-03 15:32:49 +0800

    2-2

--- 02.cpp
+++ 02.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <map>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -32,7 +33,44 @@ void part1() {
     println("{}", num2 * num3);
 }
 
+size_t hamming_distance(const string& lhs, const string& rhs) {
+    if (lhs.size() != rhs.size()) {
+        return numeric_limits<size_t>::max();
+    }
+    size_t dist = 0;
+    for (size_t i = 0; i < lhs.size(); ++i) {
+        if (lhs[i] != rhs[i]) {
+            ++dist;
+        }
+    }
+    return dist;
+}
+
+void part2() {
+    ifstream input("input/input02");
+    vector<string> id_vec;
+    for (string line; getline(input, line);) {
+        id_vec.push_back(line);
+    }
+
+    for (size_t i = 0; i < id_vec.size() - 1; ++i) {
+        for (size_t j = i + 1; j < id_vec.size(); ++j) {
+            if (hamming_distance(id_vec[i], id_vec[j]) == 1) {
+                string common;
+                for (size_t k = 0; k < id_vec[i].size(); ++k) {
+                    if (id_vec[i][k] == id_vec[j][k]) {
+                        common.push_back(id_vec[i][k]);
+                    }
+                }
+                println("{}", common);
+                break;
+            }
+        }
+    }
+}
+
 int main() {
     part1();
+    part2();
     return 0;
 }
