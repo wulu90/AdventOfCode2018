dbb2210a8056446dad4cea47e180024104099e04
wulu <wulu90@outlook.com>
2025-03-03 14:17:32 +0800

    1-2

--- 01.cpp
+++ 01.cpp
@@ -1,18 +1,48 @@
 #include <fstream>
 #include <print>
+#include <set>
+#include <vector>
 
 using namespace std;
 
 void part1() {
     ifstream input("input/input01");
-    int res = 0;
+    int fre = 0;
     for (int num; input >> num;) {
-        res += num;
+        fre += num;
     }
-    println("{}", res);
+    println("{}", fre);
+}
+
+void part2() {
+    ifstream input("input/input01");
+    int fre = 0;
+    set<int> fre_set{fre};
+    vector<int> change_vec;
+    for (int num; input >> num;) {
+        change_vec.push_back(num);
+    }
+
+    bool twice = false;
+    while (true) {
+        for (auto n : change_vec) {
+            fre += n;
+            if (fre_set.contains(fre)) {
+                println("{}", fre);
+                twice = true;
+                break;
+            } else {
+                fre_set.insert(fre);
+            }
+        }
+        if (twice) {
+            break;
+        }
+    }
 }
 
 int main() {
     part1();
+    part2();
     return 0;
 }
