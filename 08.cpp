cf63dc215696f8afb6a072867564a5c60e1624e5
wulu <wulu90@outlook.com>
2025-03-05 17:50:29 +0800

    8-2

--- 08.cpp
+++ 08.cpp
@@ -36,7 +36,21 @@ int meta_sum(node* n) {
     return sum;
 }
 
-void part1() {
+int calc_value(node* n) {
+    if (n->children.empty()) {
+        return accumulate(n->metadata.begin(), n->metadata.end(), 0, [](auto lhs, auto& rhs) { return lhs + rhs; });
+    } else {
+        int value = 0;
+        for (auto entry : n->metadata) {
+            if (entry != 0 && entry <= n->children.size()) {
+                value += calc_value(n->children[entry - 1]);
+            }
+        }
+        return value;
+    }
+}
+
+void part1_2() {
     ifstream input("input/input08");
     vector<int> license;
     for (int n; input >> n;) {
@@ -46,9 +60,10 @@ void part1() {
     node* root   = parse_tree(license, nullptr, index);
 
     println("{}", meta_sum(root));
+    println("{}", calc_value(root));
 }
 
 int main() {
-    part1();
+    part1_2();
     return 0;
 }
